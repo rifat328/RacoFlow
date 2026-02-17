b4fff326cba5e9f932151630de53566cb96aa300
Rifat Hossain <rifathossain328663@gmail.com>
2026-02-17 17:00:43 +0600

    database setup; PostgreSql with serverless Neon.tech service with drizzle ORM and drizzleKit. schema of db included

---
+++ BackEnd/DATABASE/db.js
@@ -0,0 +1,7 @@
+import "dotenv/config";
+import { drizzle } from "drizzle-orm/neon-http";
+import { neon } from "@neondatabase/serverless";
+import * as schema from "./schema.js";
+if (!process.env.DATABASE_URL) throw new Error("Database URL is required");
+const sql = neon(process.env.DATABASE_URL);
+export const db = drizzle(sql, { schema });
---
+++ BackEnd/DATABASE/schema.js
@@ -0,0 +1,61 @@
+import { varchar } from "drizzle-orm/mysql-core";
+import {
+  pgTable,
+  serial,
+  text,
+  timestamp,
+  pgEnum,
+  uuid,
+  PgUUID,
+} from "drizzle-orm/pg-core";
+
+//---Enums---
+export const roleEnum = pgEnum("role", ["ADMIN", "BUYER", "SOLVER", "USER"]);
+export const projectStatusEnum = pgEnum("project_status", [
+  "OPEN",
+  "ASSIGNED",
+  "COMPLETED",
+  "CANCEL",
+]);
+export const requestStatusEnum = pgEnum("request_status", [
+  "PENDING",
+  "ACCEPTED",
+  "REJECTED",
+]);
+export const taskStatusEnum = pgEnum("task_status", [
+  "TODO",
+  "SUBMITTED",
+  "COMPLETED",
+]);
+
+// ---Users Table---
+export const users = pgTable("users", {
+  id: uuid("id").defaultRandom().primaryKey(),
+  name: varchar("name", { length: 50 }).notNull(),
+  email: varchar("email", { length: 50 }).unique().notNull(),
+  password: varchar("password", { length: 50 }).notNull(),
+  role: roleEnum("role").default("USER"),
+  createdAt: timestamp("created_at").defaultNow(),
+});
+
+// --- Projects Table ---
+export const projects = pgTable("projects", {
+  id: serial("id").primaryKey(),
+  title: varchar("title", { length: 100 }).notNull(),
+  description: text("description").notNull(),
+  status: projectStatusEnum("status").default("OPEN"),
+  buyerId: uuid("buyer_id").references(() => users.id),
+  createdAt: timestamp("created_at").defaultNow(),
+});
+
+// --- Tasks Table ---
+export const tasks = pgTable("tasks", {
+  id: serial("id").primaryKey(),
+  projectId: serial("project_id").references(() => projects.id),
+  title: varchar("title", { length: 100 }).notNull(),
+  description: text("description"),
+  deadline: timestamp("deadline"),
+  status: taskStatusEnum("status").default("TODO"),
+  createdAt: timestamp("created_at").defaultNow(),
+  zipFileUrl: text("zip_file_url"),
+});
